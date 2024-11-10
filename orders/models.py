f9f2fca12f6fbd53d99fcdf6c00aac6eec07293d
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-10 18:01:22 +0000

    Order class

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=200)
+    quantity  = models.IntegerField()
+    order_date = models.DateTimeField(default=timezone.now)
