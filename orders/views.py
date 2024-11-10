218a2939be50880317a285033690017080fb685c
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-10 19:28:43 +0000

    creating an orders_list

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from django.utils import timezone
+from django.contrib import messages
+
+from orders.models import Order
 
 # Create your views here.
+
+
+def orders_list(request):
+    orders = Order.objects.all()
+
+    return HttpResponse(f"List of orders: {orders}")
