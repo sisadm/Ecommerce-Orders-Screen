213338f67fb6e8d919935eed7189c1d7d193648e
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-13 20:21:52 +0000

    creating the views to the urls

--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,23 @@ from orders.models import Order
 # Create your views here.
 
 
+# listing all orders
 def orders_list(request):
     orders = Order.objects.all()
+    return render(request, 'templates/orders/order_list.html', {'orders': orders})
 
-    return HttpResponse(f"List of orders: {orders}")
+# new order
+def new_order(request):
+    
+    return HttpResponse("Hello new Order")
+
+
+
+def edit_order(request):
+
+    return HttpResponse("Hello edit Order")
+
+
+def delete_order(request):
+
+    return HttpResponse("Hello delete Order")
