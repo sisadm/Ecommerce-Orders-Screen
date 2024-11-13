bb1b380aec111345ba9ace30b4222017dfedd4d7
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-13 20:21:24 +0000

    adding new urls

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.order_list, name='orders_list')
+    path('orders/', views.orders_list, name='orders_list'),
+    path('orders/create/', views.new_order, name="new_orders"),
+    path('orders/edit/', views.edit_order, name="edit_orders"),
+    path('orders/delete/', views.delete_order, name="delete_orders"),
 ]
