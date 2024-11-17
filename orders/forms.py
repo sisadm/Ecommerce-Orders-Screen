c8e3715056bcba37a20114dc59daee37c0658a54
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-17 19:50:47 +0000

    adding widget to forms

--- orders/forms.py
+++ orders/forms.py
@@ -5,3 +5,8 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['customer_name', 'product_name', 'quantity']
+        widget = {
+            'customer_name': forms.TextInput(attrs={'class': 'm-2', 'placeholder': 'Enter customer Name:'}),
+            'product_name': forms.TextInput(attrs={'class': 'm-2', 'placeholder': 'Enter product Name:'}),
+            'quantity': forms.TextInput(attrs={'class': 'm-2', 'placeholder': 'Enter quantity:'})
+        }
