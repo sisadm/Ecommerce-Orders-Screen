17e6b202775fd7850470a568104a56338d3f371d
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2024-11-17 19:53:30 +0000

    creating new path

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('orders/', views.orders_list, name='orders_list'),
     path('orders/create/', views.new_order, name="new_orders"),
-    path('orders/edit/', views.edit_order, name="edit_orders"),
-    path('orders/delete/', views.delete_order, name="delete_orders"),
+    path('orders/<int:id>/edit/', views.edit_order, name="edit_orders"),
+    path('orders/<int:id>/delete/', views.delete_order, name="delete_orders"),
 ]
